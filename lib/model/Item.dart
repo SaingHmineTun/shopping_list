2e2b5d27cda9421b863f6f9c9ad4401479c034e8
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2026-04-24 19:53:31 +0630

    Added comments for Category enum!

--- lib/model/Item.dart
+++ lib/model/Item.dart
@@ -5,10 +5,15 @@ enum Category {
   food(Colors.orange),
   // ပဵၼ်ၼမ်ႉလႄႈ သႂ်ႇသ
   drink(Colors.blue),
+  // သီလႅင် တၢင်တူဝ် သိူဝ်ႈၶူဝ်းၼုင်ႈ
   clothes(Colors.red),
+  // သီၵွၼ်းၾႆး တၢင်တူဝ် ၶိူင်ႈၸႂ်ႉတိုဝ်း
   accessories(Colors.grey),
+  // သီလိူင် တၢင်တူဝ် ၶိူင်ႈၾႆးၾႃႉ
   electronics(Colors.yellow),
+  // သီၶဵဝ် တၢင်တူဝ် ပပ်ႉလိၵ်ႈ
   books(Colors.green),
+  // သီၵမ်ႇ တၢင်တူဝ် တၢင်ႇလွင်ႈ
   others(Colors.purple);
 
   final Color color;
